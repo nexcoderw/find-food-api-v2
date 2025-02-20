1426084242a019d5161b94f5de47c18b27aae995
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-20 16:43:32 +0200

    Create logout view that blacklists the refresh token for user logout

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ app_name = 'auth'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
